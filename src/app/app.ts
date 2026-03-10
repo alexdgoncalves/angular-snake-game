ca0eded0823363a11ccd90bfb59333ed58f643b5
Alex Goncalves <alex.d.goncalves21@gmail.com>
2026-03-10 00:40:45 -0300

    :bug: fix: correcao para build do projeto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { GridComponent } from "./game/components/grid/grid";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, GridComponent],
+  imports: [GridComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
